77c68d6962ac11f27e4fa83917568c63e80b18e6
Sheldon Reddy <sheldon.reddy@gmail.com>
2026-03-21 10:06:12 +0200

    Sets up application and unit tests
    Application: Console App .NET 10 (LTS)
    Unit Tests: Xunit

---
+++ LifeApp.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace LifeApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ LifeApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace LifeApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
