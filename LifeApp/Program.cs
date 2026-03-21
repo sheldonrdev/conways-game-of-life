6083856a201fe7a52229ddf18688ec79e6eccd64
Sheldon Reddy <sheldon.reddy@gmail.com>
2026-03-21 21:55:39 +0200

    Completes basic flow without grid generation

--- LifeApp/Program.cs
+++ LifeApp/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace LifeApp;
+﻿using LifeApp.Core.Engine;
+using LifeApp.Core.Grid;
+
+namespace LifeApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var gameEngine = new GameEngine();
+        
+        bool[,] grid =
+        {
+            { false, true,  false },
+            { false, false, false  },
+            { true,  true,  true  },
+            { false, false, false }
+        };
+
+        Console.WriteLine("Generation 0:");
+        GridDisplay.Output(grid);
+
+        var gridNextGen = gameEngine.GetNextGeneration(grid);
+
+        Console.WriteLine("Generation 1:");
+        GridDisplay.Output(gridNextGen);
     }
 }
