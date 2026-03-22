fb08240566d1a7c1cad6cdda1d3aabc7640ca25c
Sheldon Reddy <sheldon.reddy@gmail.com>
2026-03-22 09:03:53 +0200

    Adds continuous play

--- LifeApp/Program.cs
+++ LifeApp/Program.cs
@@ -7,23 +7,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter number of rows: ");
-        var rows = int.Parse(Console.ReadLine()!);
-        Console.Write("Enter number of columns: ");
-        var cols = int.Parse(Console.ReadLine()!);
-        Console.Write("Enter number of generations: ");
-        var generations = int.Parse(Console.ReadLine()!);
-
         var gameEngine = new GameEngine();
-        var grid = GridFactory.CreateRandomGrid(rows, cols);
 
-        for (var gen = 0; gen < generations; gen++)
+        do
         {
-            Console.Clear();
-            Console.WriteLine($"Generation {gen}:");
-            GridDisplay.Output(grid);
-            Thread.Sleep(500);
-            grid = gameEngine.GetNextGeneration(grid);
-        }
+            Console.Write("Enter number of rows: ");
+            var rows = int.Parse(Console.ReadLine()!);
+            Console.Write("Enter number of columns: ");
+            var cols = int.Parse(Console.ReadLine()!);
+            Console.Write("Enter number of generations: ");
+            var generations = int.Parse(Console.ReadLine()!);
+
+            var grid = GridFactory.CreateRandomGrid(rows, cols);
+
+            for (var gen = 0; gen < generations; gen++)
+            {
+                Console.Clear();
+                Console.WriteLine($"Generation {gen}:");
+                GridDisplay.Output(grid);
+                Thread.Sleep(500);
+                grid = gameEngine.GetNextGeneration(grid);
+            }
+
+            Console.WriteLine("Continue game play? (y/n): ");
+        } 
+        while (Console.ReadLine()?.Trim().ToLower() == "y");
+        
+        Console.WriteLine("Thanks for playing :)");
+        Thread.Sleep(500);
     }
 }
